fc9186776ef24687bccc22932b59ec27179f7f02
Carlos Reyes <carlos_mlb_reyes@hotmail.com>
2021-11-06 22:21:57 -0600

    Commit de la lección 12 del curso de Angular 8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,57 +11,57 @@ export class AppComponent {
     inputItem = '';
     
     items = ['Carlos', 'Andres', 'Reyes', 'Villacorta', 1996]
-
+    
     products: Product[] = [
         {
-          id: '1',
-          image: 'assets/images/tv.jpg',
-          title: 'TV',
-          price: 80000,
-          description: 'bla bla bla bla bla'
+            id: '1',
+            image: 'assets/images/tv.jpg',
+            title: 'TV',
+            price: 80000,
+            description: 'bla bla bla bla bla'
         },
         {
-          id: '2',
-          image: 'assets/images/rubik.png',
-          title: 'Cubo Rubik',
-          price: 80000,
-          description: 'bla bla bla bla bla'
+            id: '2',
+            image: 'assets/images/rubik.png',
+            title: 'Cubo Rubik',
+            price: 80000,
+            description: 'bla bla bla bla bla'
         },
         {
-          id: '3',
-          image: 'assets/images/cr7.jpeg',
-          title: 'El Bicho',
-          price: 80000,
-          description: 'bla bla bla bla bla'
+            id: '3',
+            image: 'assets/images/cr7.jpeg',
+            title: 'El Bicho',
+            price: 80000,
+            description: 'bla bla bla bla bla'
         },
         {
-          id: '4',
-          image: 'assets/images/ps5.jpg',
-          title: 'Consola PS5',
-          price: 80000,
-          description: 'bla bla bla bla bla'
+            id: '4',
+            image: 'assets/images/ps5.jpg',
+            title: 'Consola PS5',
+            price: 80000,
+            description: 'bla bla bla bla bla'
         },
         {
-          id: '5',
-          image: 'assets/images/carro.jpg',
-          title: 'Carro ultimo modelo',
-          price: 80000,
-          description: 'bla bla bla bla bla'
+            id: '5',
+            image: 'assets/images/carro.jpg',
+            title: 'Carro ultimo modelo',
+            price: 80000,
+            description: 'bla bla bla bla bla'
         },
         {
-          id: '6',
-          image: 'assets/images/pc.jpg',
-          title: 'PC de oficina',
-          price: 80000,
-          description: 'bla bla bla bla bla'
+            id: '6',
+            image: 'assets/images/pc.jpg',
+            title: 'PC de oficina',
+            price: 80000,
+            description: 'bla bla bla bla bla'
         }
-      ];
-
+    ];
+    
     addItem(){
         this.items.push(this.inputItem)
         this.inputItem = '';
     }
-
+    
     deleteItem(index: number){
         this.items.splice(index, 1)
     }
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
 
 @Component({
-  selector: 'app-product',
-  templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+    selector: 'app-product',
+    templateUrl: './product.component.html',
+    styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit {
+    
+    constructor() { }
+    
+    ngOnInit(): void {
+    }
 
-  constructor() { }
+    product: Product = {
+        id: '1',
+        image: 'assets/images/tv.jpg',
+        title: 'TV',
+        price: 80000,
+        description: 'bla bla bla bla bla'
+    }
 
-  ngOnInit(): void {
-  }
-
+        
 }
+    
